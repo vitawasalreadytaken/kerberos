9b4d33a3ed1c3edc71b171ae5b723c6a464eb003
Vita Smid <me@ze.phyr.us>
2014-01-06 18:49:30 +0800

    Improved default settings

--- settings.py
+++ settings.py
@@ -1,19 +1,21 @@
+TWITTER_AUTH = {
+	'CONSUMER_TOKEN': 'get this from dev.twitter.com',
+	'CONSUMER_SECRET': 'get this from dev.twitter.com',
+	'ACCESS_TOKEN_KEY': 'get this from dev.twitter.com',
+	'ACCESS_TOKEN_SECRET': 'get this from dev.twitter.com',
+}
+
+DM_RECIPIENTS = ['twitter_username_here', 'another_username']
+
+
 import os
 LOG_FILE = os.path.join(os.path.dirname(__file__), 'kerberos.log')
 
 WATCH_LOG = '/var/log/auth.log'
 MATCH_LINES = r'Accepted \S+ for \S+ from \S+'
 
-TWITTER_AUTH = {
-	'CONSUMER_TOKEN': '',
-	'CONSUMER_SECRET': '',
-	'ACCESS_TOKEN_KEY': '',
-	'ACCESS_TOKEN_SECRET': '',
-}
 
-DM_RECIPIENTS = []
-
-
+# Try to import user overrides from settings_local.py.
 try:
 	from settings_local import *
 except ImportError:
